cca7fe087e5a9fea02cad0a2026e75ba76556b8f
Christopher Kumm <chrislkumm@protonmail.com>
2023-11-15 13:38:22 -0600

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  // https://vitejs.dev/config/#using-environment-variables-in-config
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+    server: {
+      port: 3000,
+    },
+    preview: {
+      port: 3000,
+    },
+  }
+})
