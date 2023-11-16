ebcdb8a897fcf12238ef4c68fba9d854fafa4341
Christopher Kumm <chrislkumm@protonmail.com>
2023-11-16 14:30:42 -0600

    Ignore linting error
    Most of the app doesn't need Node types since it's meant to run in a
    browser. However, the Vite config is an exception. Rather than install
    Node types everywhere, we'll just ignore the missing type for `process`.

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
   // https://vitejs.dev/config/#using-environment-variables-in-config
+  // @ts-expect-error Exception for missing `process` type in Node environment.
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
